be989a15675523c078fb35d9770128b7fe1482c0
bhavanawagh <bhavanawagh3011@gmail.com>
2020-05-24 23:12:48 +0530

    Added a const client to get access of space and token, and given it a space ID and token from Contentful,A space is like a project folder in contentful terms.Normaly we get both ID and the token in the Contentful web app.Also use this client const by applying getEntries method on it using async/await.And its done.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "9scrxi9kow5q",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "lQrdGo6F0Bt46vNQY3eAWUWbcFxyFgCMWD8g1IfRW4U"
+});
+
+
 //variables
 
 const cartBtn = document.querySelector(".cart-btn");
@@ -20,10 +28,15 @@ let buttonsDOM = [];
 class Products{
   async getProducts(){
     try {
-       let result = await fetch('products.json');
-       let data = await result.json();
 
-       let products = data.items;
+      let contentful = await client.getEntries({
+        content_type: "shoppingCartProductExmple"
+      });
+        
+       //let result = await fetch('products.json');
+       //let data = await result.json();
+
+       let products = contentful.items;
        products = products.map(item => {
           const {title,price} = item.fields;
           const {id} = item.sys;
@@ -237,4 +250,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
      ui.cartLogic();
    });
 });
+
+
 
